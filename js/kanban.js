e0c302f857c1e4a96f0ff5813919e6bb67b3df01
Ankit Kumar <codeaught@gmail.com>
2022-02-06 00:00:22 +0530

    feat: kanban board events

--- js/kanban.js
+++ js/kanban.js
@@ -15,8 +15,56 @@ add_btn.addEventListener('click', () => {
   }
 });
 
+const onDragStart = (event) => {
+  return event.dataTransfer.setData('text', event.target.id);
+};
+
+const onDragEnd = (event) => {
+  return event.dataTransfer.clearData();
+};
+
+const onDrop = (event) => {
+  event.preventDefault();
+  const id = event.dataTransfer.getData('text');
+  event.target.append(document.getElementById(id));
+}
+
+const onSave = () => {
+  error.innerHTML = '';
+  const input = document.querySelector('input');
+  const value = input.value;
+  if (value !== '') {
+    const item = document.getElementById('item' + order);
+    order += 1;
+    item.append(value);
+    adding = false;
+  } else {
+    error.innerHTML = message;
+  }
+};
+
 const create_item = () => {
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.id = 'item' + order;
+  item.draggable = true;
+
+  item.addEventListener('dragstart', onDragStart);
+  item.addEventListener('dragend', onDragEnd);
+
+  const input = document.createElement('input');
+  item.append(input);
+
+  const save_btn = document.createElement('button');
+  save_btn.innerHTML = 'Save';
+
+  save_btn.addEventListener('click', onSave);
+  item.append(save_btn);
+
+  return item;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', onDrop);
+  element.addEventListener('dragover', event => event.preventDefault());
 });
